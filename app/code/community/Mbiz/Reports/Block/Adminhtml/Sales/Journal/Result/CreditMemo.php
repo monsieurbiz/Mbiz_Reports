cccee2c540f6fa2c166a03f88e1fe014b9795dbd
Mina Amrouche <m.amrouche@monsieurbiz.com>
2017-04-04 18:29:46 +0200

    Create `CreditMemo` block

---
+++ app/code/community/Mbiz/Reports/Block/Adminhtml/Sales/Journal/Result/CreditMemo.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * This file is part of Mbiz_Reports for Magento.
+ *
+ * @license All rights reserved
+ * @author Mina Amrouche <m.amrouche@monsieurbiz.com> <@mina_amrouche>
+ * @category Mbiz
+ * @package Mbiz_Reports
+ * @copyright Copyright (c) 2017 Monsieur Biz (https://monsieurbiz.com/)
+ */
+
+/**
+ * Adminhtml_Sales_Journal_Result_CreditMemo Block
+ * @package Mbiz_Reports
+ */
+class Mbiz_Reports_Block_Adminhtml_Sales_Journal_Result_CreditMemo extends Mbiz_Reports_Block_Adminhtml_Sales_Journal_Result_Abstract
+{
+
+// Monsieur Biz Tag NEW_CONST
+
+// Monsieur Biz Tag NEW_VAR
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTitle()
+    {
+        return $this->__('Credit Memos');
+    }
+
+    /**
+     * Retrieve the columns (code => name)
+     * @return array
+     */
+    public function getColumns()
+    {
+        return [
+            "total_credit_memo" => $this->__('Credit Memos Total Amount'),
+        ];
+    }
+
+    /**
+     * Format a value
+     * @param string $columnCode
+     * @param string $value
+     * @return string The value formated
+     */
+    public function formatValue($columnCode, $value)
+    {
+        return $this->formatPrice($value);
+    }
+
+// Monsieur Biz Tag NEW_METHOD
+
+}
---
+++ app/code/community/Mbiz/Reports/Model/Sales/Journal/Result/CreditMemo.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This file is part of MBiz_Reports for Magento.
+ *
+ * @license All rights reserved
+ * @author Mina Amrouche <m.amrouche@monsieurbiz.com> <@mina_amrouche>
+ * @category MBiz
+ * @package MBiz_Reports
+ * @copyright Copyright (c) 2017 Monsieur Biz (https://monsieurbiz.com/)
+ */
+
+/**
+ * Sales_Journal_Result_CreditMemo Model
+ * @package MBiz_Reports
+ */
+class MBiz_Reports_Model_Sales_Journal_Result_CreditMemo extends Mbiz_Reports_Model_Sales_Journal_Result_Abstract
+{
+
+// Monsieur Biz Tag NEW_CONST
+
+// Monsieur Biz Tag NEW_VAR
+
+    protected function _load(\Mbiz_Reports_Model_Sales_Journal_Request $request, \Zend_Date $from, \Zend_Date $to)
+    {
+        $read = $this->getReadAdapter();
+        $select = $read->select();
+        $select
+            ->from([
+                'memo' => $this->getTableName('sales/creditmemo')
+            ], [
+                'total_credit_memo' => 'SUM(ROUND(memo.grand_total, 2))',
+            ])
+            ->where("DATE(memo.created_at) >= ?", $from->toString('y-MM-dd'))
+            ->where("DATE(memo.created_at) <= ?", $to->toString('y-MM-dd'))
+        ;
+
+        $results = $select->query()->fetch();
+        return [new Varien_Object($results)];
+    }
+
+// Monsieur Biz Tag NEW_METHOD
+
+}
