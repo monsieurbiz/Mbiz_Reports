dc3e7d9f80d1a7c46b8d04351baba127c7331f17
Mina Amrouche <m.amrouche@monsieurbiz.com>
2017-04-12 12:01:14 +0200

    Add `Total` line to TVA block

--- app/code/community/Mbiz/Reports/Block/Adminhtml/Sales/Journal/Result/Tva.php
+++ app/code/community/Mbiz/Reports/Block/Adminhtml/Sales/Journal/Result/Tva.php
@@ -34,8 +34,8 @@ class Mbiz_Reports_Block_Adminhtml_Sales_Journal_Result_Tva extends Mbiz_Reports
     public function getColumns()
     {
         return [
-            'rate' => $this->__("TVA"),
             'title' => $this->__("Title"),
+            'rate' => $this->__("TVA"),
             'nb_orders' => $this->__("Number of orders"),
             'amount' => $this->__("Montant"),
         ];
@@ -54,6 +54,30 @@ class Mbiz_Reports_Block_Adminhtml_Sales_Journal_Result_Tva extends Mbiz_Reports
             default:
                 return $value;
         }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getItems()
+    {
+        $total = new Varien_Object([
+            'title' => '<strong>TOTAL</strong>',
+            'rate' => null,
+            'nb_orders' => 0,
+            'amount' => 0,
+        ]);
+        $items = parent::getItems();
+
+        foreach ($items as $item) {
+            $total
+                ->setNbOrders($total->getNbOrders() + $item->getNbOrders())
+                ->setAmount($total->getAmount() + $item->getAmount())
+            ;
+        }
+        $items['total'] = $total;
+
+        return $items;
     }
 
 // Monsieur Biz Tag NEW_METHOD
